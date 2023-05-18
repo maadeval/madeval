d86326f24fe573d25f7bf7dc49b81d417361cf55
madeval <alvarez.fing@gmail.com>
2023-05-17 22:36:21 -0300

    chore: :arrow_up: update dependencies and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
-import preact from '@astrojs/preact'
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
-})
+  integrations: [preact(), image()]
+});
