5ac6aad94987df530e371dd7188cd36b7633a3b7
madeval <alvarez.fing@gmail.com>
2023-05-17 22:38:13 -0300

    chore: :wrench: set handler of images by astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
 
-import image from "@astrojs/image";
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), image()]
-});
+  integrations: [preact(), image()],
+})
